b9519af4871f38fe39d4a6013d25744afdc310a5
RobWar-code <rowaniar@aol.com>
2023-04-02 18:24:47 +0000

    Deployment changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn xbasic_promo.wsgi
---
+++ xb_promo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class XbPromoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'xb_promo'
