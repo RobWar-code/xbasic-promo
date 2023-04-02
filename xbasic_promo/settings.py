98f8f53d070590d8e5dfe07f6117478f6c75fa8d
RobWar-code <rowaniar@aol.com>
2023-04-02 18:54:22 +0000

    fix cloudinary_storage reference in settings.py

--- xbasic_promo/settings.py
+++ xbasic_promo/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary.storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'xb_promo'
