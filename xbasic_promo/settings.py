0347a77612b115bbde54d436e11753839437102f
RobWar-code <rowaniar@aol.com>
2023-04-02 18:57:37 +0000

    fix ALLOWED_HOSTS in settings.py

--- xbasic_promo/settings.py
+++ xbasic_promo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['xbasic_promo.herokuapp.com']
+ALLOWED_HOSTS = ['xbasic-promo.herokuapp.com']
 
 
 # Application definition
