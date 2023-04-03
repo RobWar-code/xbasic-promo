b212319efcd49a6a1729c7134131ea5e74a57bf7
RobWar-code <rowaniar@aol.com>
2023-04-03 15:02:16 +0000

    Add initial cut at page admin and presentation

--- xb_promo/admin.py
+++ xb_promo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import FeaturePage
 
 # Register your models here.
+admin.site.register(FeaturePage)
--- xb_promo/views.py
+++ xb_promo/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import FeaturePage
+
 
 # Create your views here.
+class Feature(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = FeaturePage.objects.all()
+        feature_page = get_object_or_404(queryset, slug=slug)
+        return render(
+            request,
+            'index.html',
+            {
+                'feature_page': feature_page
+            }
+        )
