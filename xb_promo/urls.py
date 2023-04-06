047879590935395c8c4dcd241914854f3347784d
RobWar-code <rowaniar@aol.com>
2023-04-06 09:34:45 +0000

    Add GUI-design page

--- xb_promo/urls.py
+++ xb_promo/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Feature.as_view(), name='home')
+    path('', views.Feature.as_view(), name='home'),
+    path('<slug:slug>', views.Feature.as_view(), name='feature_page')
 ]
