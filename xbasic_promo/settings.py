43850a7e911192a303b5e7c26aa2809d71d49316
RobWar-code <rowaniar@aol.com>
2023-04-14 10:54:53 +0000

    Fix debug again

--- xbasic_promo/settings.py
+++ xbasic_promo/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 if os.path.isfile("env.py"):
     import env
 
-os.environ.setdefault('DEBUG', 'True')
+DEBUG = os.environ.get('DEBUG', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
